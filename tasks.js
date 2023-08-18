d93c3d3e4a8c954aa95d2c01a5cf0551def49d61
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-08-18 11:37:14 +0500

    binarySearch

--- tasks.js
+++ tasks.js
@@ -57,3 +57,30 @@ function eachCons(array, n) {
 //     each_cons([1,2,3,4], 3)
 // #=> [[1,2,3],[2,3,4]]
 }
+
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const binarySearch = (arr, item) => {
+    let start = 0
+    let end = arr.length
+    let middle;
+    let found = false
+    let position = -1
+    while (found === false && start <= end) {
+        middle = Math.floor((start + end) / 2)
+        if (arr[middle] === item) {
+            found = true
+            position = middle
+            return position
+        }
+        if (item < arr[middle]) {
+            end = middle - 1
+        } else {
+            start = middle + 1
+        }
+
+    }
+    return position
+}
+
