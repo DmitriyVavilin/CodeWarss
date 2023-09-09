f1c42e2aa1b010172982ca7ae20c2781a4697147
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-09-09 21:14:32 +0500

    maxNumber and minNumber

--- tasks.js
+++ tasks.js
@@ -282,3 +282,17 @@ function validParentheses(str) {
 // "("               =>  false
 // "(())((()())())"  =>  true
 }
+
+
+const minNumber = function(list){
+    const maxNum = Math.max(...list)
+    return maxNum
+}
+
+const maxNumber = function(list){
+    const minNum = Math.min(...list)
+    return minNum
+    //Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
+    // that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+}
+
