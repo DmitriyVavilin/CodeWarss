4c0854c772a75d8d6c14eb941af4f7e7bb084110
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-09-12 23:28:41 +0500

    countPositivesSumNegatives

--- tasks.js
+++ tasks.js
@@ -296,3 +296,29 @@ const maxNumber = function(list){
     // that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 }
 
+function countPositivesSumNegatives(arr) {
+    if (!arr || arr.length === 0) {
+        return [];
+    }
+
+    let positiveCount = 0;
+    let sumNegative = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveCount++;
+        } else if (arr[i] < 0) {
+            sumNegative += arr[i];
+        }
+    }
+
+    return [positiveCount, sumNegative];
+    // Given an array of integers.
+    //
+    //     Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+    //
+    //     If the input is an empty array or is null, return an empty array.
+    //
+    //     Example
+    // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+}
