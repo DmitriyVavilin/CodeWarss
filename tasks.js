5b88b55999d55e7fdb773a2bef88ca70466ee907
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-09-17 23:38:49 +0500

    numbersWithDigitInside

--- tasks.js
+++ tasks.js
@@ -339,3 +339,18 @@ function sumOfIntegersInString(string) {
     // Note: only positive integers will be tested.
 }
 
+function numbersWithDigitInside(x, d) {
+    let count = 0;
+    let sum = 0;
+    let product = 1;
+
+    for (let i = 1; i <= x; i++) {
+        if (i.toString().includes(d.toString())) {
+            count++;
+            sum += i;
+            product *= i;
+        }
+    }
+
+    return count === 0 ? [0, 0, 0] : [count, sum, product];
+}
