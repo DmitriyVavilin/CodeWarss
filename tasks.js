255eec3bbdd9cfbd531987d26170b8744f11c0a7
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-10-12 17:58:23 +0500

    paperwork

--- tasks.js
+++ tasks.js
@@ -474,3 +474,11 @@ function fakeBin(x){
     x=x.replace(/[5-9]/g,'1')
     return x
 }
+
+function paperwork(n, m) {
+    if(n <= 0 | m <=0){
+        return 0
+    }else {
+        return n * m
+    }
+}
