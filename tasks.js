9ad15950171913c41e12997b9764b2151dd8d610
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-10-16 23:15:22 +0500

    eachCons

--- tasks.js
+++ tasks.js
@@ -482,3 +482,13 @@ function paperwork(n, m) {
         return n * m
     }
 }
+
+function eachCons(arr, n) {
+    const result = [];
+
+    for (let i = 0; i <= arr.length - n; i++) {
+        result.push(arr.slice(i, i + n));
+    }
+
+    return result;
+}
